a1f6ef07ce36bb143a6b9d8753568bd84480b535
Kurome <hunt31999@gmail.com>
2025-04-11 14:44:36 +0900

    add emacs-outli

--- emacs-xyz.scm
+++ emacs-xyz.scm
@@ -80,4 +80,27 @@ completion frameworks.")
  for Emacs, enabling smooth and in-place scrolling functionality.")
     (license license:gpl3+)))
 
-
+(define-public emacs-outli
+  (package
+    (name "my-emacs-outli")
+    (version "0.2.3")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/jdtsmith/outli")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "01q37gbfc0mal3ha2x5a1axdcd2c0d55imav4w0cfzs3sm48azvs"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/jdtsmith/outli")
+    (synopsis "Simple and stylish comment-based outliner for Emacs")
+    (description "This package provides @code{outli}, a minimal and elegant
+outliner for Emacs that enhances @code{outline-minor-mode} with configurable
+heading syntax, styled headings, and org-mode-inspired navigation and structure
+editing. It supports comment-based headers with customizable stems and repeat
+characters, styled overlines and backgrounds, tab-based visibility toggling,
+org-style speed keys for headline manipulation, and imenu integration for fast
+navigation.")
+    (license license:gpl3+)))
